39d30a69948ee4354a890f644ae75895ca4561c4
Reuben John <reubenvjohn@gmail.com>
2024-03-10 00:01:43 +0000

    Configured /api/ai/* redirect to AI server API

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/ai/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5328/api/:path*'
+            : `${process.env.AI_REST_API_URL}/:path*`,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
